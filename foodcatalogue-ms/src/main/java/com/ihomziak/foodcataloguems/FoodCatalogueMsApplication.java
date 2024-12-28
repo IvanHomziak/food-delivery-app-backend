d5450067610fff92e627ff4c2c55aa224d466f86
ihomz <IVAN.HOMZIAK@ONEDATASCAN.COM>
2024-12-28 10:37:18 +0100

    foodcatalogue-ms

---
+++ foodcatalogue-ms/src/main/java/com/ihomziak/foodcataloguems/FoodCatalogueMsApplication.java
@@ -0,0 +1,13 @@
+package com.ihomziak.foodcataloguems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCatalogueMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodCatalogueMsApplication.class, args);
+    }
+
+}

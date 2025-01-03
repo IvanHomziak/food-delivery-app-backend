0847e35597287bdb1bd2b48cfbc36c29b601ad00
ivanhomziak <ivan.homziak@gmail.com>
2025-01-04 00:32:37 +0100

    ref

--- user-ms/src/main/resources/scripts/script.sql
+++ user-ms/src/main/resources/scripts/script.sql
@@ -6,19 +6,19 @@ CREATE DATABASE IF NOT EXISTS userdb;
 USE userdb;
 
 -- Drop the table if it exists
-DROP TABLE IF EXISTS 'user';
+DROP TABLE IF EXISTS user;
 
 -- Create the user table
 CREATE TABLE IF NOT EXISTS user (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL,
-    address VARCHAR(255),
-    city VARCHAR(100)
-    );
+                                    user_id INT AUTO_INCREMENT PRIMARY KEY,
+                                    user_name VARCHAR(255) NOT NULL,
+                                    user_password VARCHAR(255) NOT NULL,
+                                    address VARCHAR(255),
+                                    city VARCHAR(100)
+);
 
 
 
 -- Insert data into the `food_item` table
 INSERT INTO user (user_id, user_name, user_password, address, city) VALUES
-(1, 'Jack Sparrow', 'password', '28th Ave', 'New York')
+    (1, 'Jack Sparrow', 'password', '28th Ave', 'New York')
